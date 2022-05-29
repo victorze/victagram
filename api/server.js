d40aa3056f097df40c727e1885702d6432849559
Victor Zevallos <victorze@protonmail.com>
2022-05-29 18:14:27 -0500

    Add HTTP request logger

--- api/server.js
+++ api/server.js
@@ -1,10 +1,17 @@
 require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
+const morgan = require('morgan')
 
-const { notFound, productionErrors } = require('./handlers')
+const { notFound, productionErrors, logger } = require('./handlers')
 
 const app = express()
+app.use(express.json())
+app.use(morgan('short', {
+  stream: {
+    write: message => logger.info(message.trim())
+  }
+}))
 
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('Connected successfully to mongodb server'))
@@ -12,8 +19,6 @@ mongoose.connect(process.env.MONGODB_URI)
     console.error(`mongoose error → ${err.message}`)
     process.exit(1)
   })
-
-app.use(express.json())
 
 app.get('/', (req, res) => {
   console.log('home')
