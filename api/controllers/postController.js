954083028d733baf62296be3cb9b8b39b5834549
Victor Zevallos <victorze@protonmail.com>
2022-05-30 15:19:53 -0500

    Upload post image

--- api/controllers/postController.js
+++ api/controllers/postController.js
@@ -1,6 +1,5 @@
-
 const { Post } = require('../models')
-const { catchErrors, logger } = require('../handlers')
+const { catchErrors, saveImage, logger } = require('../handlers')
 
 const store = async (req, res) => {
   const { imageUrl, caption } = req.body
@@ -10,6 +9,13 @@ const store = async (req, res) => {
   res.status(201).json(post)
 }
 
+const upload = async (req, res) => {
+  imageUrl = await saveImage(req.body, req.fileName)
+  logger.info(`El usuario '${req.user.username}' ha subido una imagen de post ${imageUrl}`)
+  res.json({ url: imageUrl })
+}
+
 module.exports = {
-  store: catchErrors(store)
+  store: catchErrors(store),
+  upload: catchErrors(upload),
 }
--- api/routes/postRoutes.js
+++ api/routes/postRoutes.js
@@ -1,7 +1,9 @@
 const route = require('express').Router()
 const { postController } = require('../controllers')
+const { validateImage } = require('../controllers/validators/image')
 const { auth } = require('../handlers')
 
 route.post('/', auth, postController.store)
+route.post('/upload', [auth, validateImage], postController.upload)
 
 module.exports = route
