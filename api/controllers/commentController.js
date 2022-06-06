aa2098d181c5b5c4c6fce221b5a8a26c7c359aab
Victor Zevallos <victorze@protonmail.com>
2022-06-05 22:52:18 -0500

    Fix comment post

--- api/controllers/commentController.js
+++ api/controllers/commentController.js
@@ -2,10 +2,10 @@ const { Post } = require("../models")
 const { catchErrors, logger } = require("../handlers")
 
 const store = async (req, res) => {
-  const { author, message } = req.body
+  const { message } = req.body
   const post = await Post.findById(req.params.id)
 
-  post.comments.push({ author, message })
+  post.comments.push({ author: req.user, message })
   const updatedPost = await post.save()
   const comment = updatedPost.comments[updatedPost.comments.length - 1]
   logger.info(`El usuario '${req.user.username}' comentó el post con id '${req.params.id}'`)
